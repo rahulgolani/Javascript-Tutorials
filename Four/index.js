fbe65e183cdc20adfcada1f3251f0467d03946d4
Rahul Golani <rahulgolani24@live.com>
2020-06-05 22:44:39 +0530

    Data Types

--- Four/index.js
+++ Four/index.js
@@ -1,4 +1,3 @@
-
 /*
 javascript is a dynamically typed language
 
@@ -7,52 +6,55 @@ data types in javascript
 2) reference [derived] [objects] [memory allocations are in heap (dynamic)]
 
  primitive data types->
-string (collection of characters) , numbers, boolean (true/false) , null(intentional empty value), undefined (default value), symbol(new intriduced in ES6)
+string (collection of characters) , number, boolean (true/false) , null(intentional empty value), undefined (default value), symbol(new introduced in ES6)
 
 reference data types->
 arrays, object literals, functions,dates
 
 */
 //string
-var name='Rahul';
-console.log('My name is '+ name);
+var name = 'Rahul';
+console.log('My name is ' + name);
 
 //typeof operator - tells the datatype of the variable
 //usage typeof variable
 console.log(typeof name);
 
-var age=23;
+var age = 23;
 console.log(typeof age);
 
-var isGraduate=true;
+var isGraduate = true;
 console.log(typeof isGraduate);
 
-var isNull=null;
+var isNull = null;
 console.log(typeof isNull); //null has a bogus return value It is a primitive datatype but its return value is object
 
-var undef=undefined
+var undef = undefined
 console.log(typeof undef);
 
 
 ////Reference data types
 
 //arrays
-var arr=[1,2,3,4,true,'hello']//can contain any primitive datatype
+var arr = [1, 2, 3, 4, true, 'hello'] //can contain any primitive datatype
 console.log(arr);
-console.log( typeof arr);//reference datatypes have type object
+console.log(typeof arr); //reference datatypes have type object
 
 //object literals
 
-let marks={a:90, b:34,c:89}
+let marks = {
+  a: 90,
+  b: 34,
+  c: 89
+}
 console.log(marks);
 console.log(typeof marks);
 
-function getName()
-{
+function getName() {
 
 }
 
-console.log(typeof getName);//type of function is function
+console.log(typeof getName); //type of function is function
 
-let date=new Date();
-console.log(typeof date);//object
+let date = new Date();
+console.log(typeof date); //object
