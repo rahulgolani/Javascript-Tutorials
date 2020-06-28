a50b98c53b3e0a540be2fc94253bf5a4e30bc197
Rahul Golani <rahulgolani24@live.com>
2020-06-28 18:30:23 +0530

    Scoping

--- Ten/tut10.js
+++ Ten/tut10.js
@@ -1,35 +1,35 @@
 /*Functions and Scope*/
 
-function greet(name){
+function greet(name) {
 
   console.log(`Good Morning ${name}`);
-  return 1;//without this this will return undefined
+  return 1; //without this this will return undefined
 }
 
-let name='Rahul'
+let name = 'Rahul'
 greet(name);
-name='XYZ'
+name = 'XYZ'
 greet(name);
 
 //default argument
 
-function greetMe(name,thank='Thank You'){
+function greetMe(name, thank = 'Thank You') {
   console.log(`Good Morning ${name} and ${thank}`);
 }
 
-greetMe(name,'Thanks')
+greetMe(name, 'Thanks')
 greetMe(name)
 
 //creating function in variable
-let mygreet=function(name){
+let mygreet = function(name) {
   console.log(`Good Evening ${name}`);
 }
 mygreet('Rahul')
 
 //can use functions in objects as well
-const obj={
-  name:'Rahul',
-  game:function(){ //Anonymous function
+const obj = {
+  name: 'Rahul',
+  game: function() { //Anonymous function
     return "PUBG";
   }
 }
@@ -37,17 +37,16 @@ const obj={
 console.log(obj.game());
 
 /*Scope*/
-var i=10
+var i = 10
 
 {
-  let i=900; //now if i remove the upper var there will be error since i is limited to this scope only and there will be no variable named i at line 46
+  let i = 900; //now if i remove the upper var there will be error since i is limited to this scope only and there will be no variable named i at line 46
   console.log(i);
 }
-
 console.log(i);
 
-function getName(name){
-  var i=9//local to the scope if declared a new variable using var
+function getName(name) {
+  var i = 9 //local to the scope if declared a new variable using var
   //but if not used var then it will modify the global variable
   //same goes with let
   console.log(i);
