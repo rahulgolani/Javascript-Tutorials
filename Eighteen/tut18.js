ca7704fba4f3a1c77afd0428a42983d0570b0ca8
Rahul Golani <rahulgolani24@live.com>
2020-06-28 22:50:36 +0530

    Events

--- Eighteen/tut18.js
+++ Eighteen/tut18.js
@@ -1,22 +1,21 @@
-
 /*MORE ON EVENTS*/
 
 //here in the html page we have used a form in which there is a button. when we click the button we are redirected to youtube.com as defined in the form action so we have to bypass the default behaviour using Javascript
 
-let btn=document.getElementById('btn');//grabbing the element
+let btn = document.getElementById('btn'); //grabbing the element
 console.log(btn);
 
-btn.addEventListener('click',func1);//adding a event listener
+btn.addEventListener('click', func1); //adding a event listener
 
 function func1(e) {
-  e.preventDefault();//to bypass the default beaviour
+  e.preventDefault(); //to bypass the default beaviour
   console.log('Thanks');
   console.log(e);
 }
 
 //double click event
 
-btn.addEventListener('dblclick',func2);
+btn.addEventListener('dblclick', func2);
 
 function func2(e) {
   console.log('Thanks its a double click');
@@ -24,8 +23,8 @@ function func2(e) {
 
 //mousedown and mouseup event
 
-btn.addEventListener('mousedown',func3);
-btn.addEventListener('mouseup',func4);
+btn.addEventListener('mousedown', func3);
+btn.addEventListener('mouseup', func4);
 
 function func3(e) {
   e.preventDefault();
@@ -41,11 +40,11 @@ function func4(e) {
 
 //mouseenter and mouseleave event
 
-let li=document.querySelector('#l');
+let li = document.querySelector('#l');
 console.log(li);
 
-li.addEventListener('mouseenter',func5);
-li.addEventListener('mouseleave',func6);
+li.addEventListener('mouseenter', func5);
+li.addEventListener('mouseleave', func6);
 
 function func5(e) {
   console.log('Thanks its a mouse enter');
@@ -57,10 +56,10 @@ function func6(e) {
 
 //mousemove event
 
-document.querySelector('.container').addEventListener('mousemove',func7);
+document.querySelector('.container').addEventListener('mousemove', func7);
 
 function func7(e) {
-  console.log(e.offsetX,e.offsetY);
+  console.log(e.offsetX, e.offsetY);
   console.log('You moved your mouse');
-  document.body.style.backgroundColor=`rgb(${e.offsetX},${e.offsetY},${200})`;//here as the mouse moves the backgroundColor will change
-}//rgb values are from 0-255
+  document.body.style.backgroundColor = `rgb(${e.offsetX},${e.offsetY},${200})`; //here as the mouse moves the backgroundColor will change
+} //rgb values are from 0-255
